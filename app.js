9296369d78315923b2418a63cf18912dc7eb38f6
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2023-01-27 23:05:50 +0300

    asdf

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const container = document.querySelector(".container")
 const ansDiv = document.createElement("div")
 //WORDS
-var words = ["git", "foreach", "format", "status", "facebook","instagram","snapchat", "twitter", "jeff bezos", "tesla", "spacex"]
+var words = ["git", "foreach", "format", "status", "facebook","instagram","snapchat", "twitter", "jeffbezos", "tesla", "spacex"]
 //KEYS
 const keys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const randomWord = words[Math.floor(Math.random() * words.length)]
+const randomWord = words[Math.floor(Math.random() * words.length)].toUpperCase()
 const keysContainer = document.createElement("div")
 keysContainer.classList = "key-container";
 
@@ -14,6 +14,7 @@ anskeysContainer.classList = "anskey-container";
 for (let i = 0; i < randomWord.length; i++) {
     const answerKey = document.createElement("div")
     answerKey.classList = "answerkeys";
+    answerKey.innerHTML = randomWord.substring(i).substring(0,1);
     anskeysContainer.appendChild(answerKey)
 }
 //KEYS WRITE
