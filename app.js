228e01179b784bf5d6c2fae637da4b43900e6f95
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2023-01-29 01:05:19 +0300

    array fix

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const gcontainer = document.createElement("div")
 gcontainer.classList = "gamecontainer"
 const gameContainer = document.createElement("div")
 //WORDS
-var words = ["git", "foreach", "format", "status", "facebook","instagram","snapchat", "twitter", "jeffbezos", "tesla", "spacex"]
+const words = ["git", "foreach", "format", "status", "facebook","instagram","snapchat", "twitter", "jeffbezos", "tesla", "spacex"]
 //KEYS
 const keys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const randomWord = words[Math.floor(Math.random() * words.length)].toUpperCase()
+
+function randomNumb() {
+    let randomer = Math.floor(Math.random() * words.length)
+    return randomer;
+}
+
+
+
+const randomWord = words[randomNumb()].toUpperCase()
 const keysContainer = document.createElement("div")
 keysContainer.classList = "key-container";
 
@@ -18,7 +26,6 @@ for (let i = 0; i < randomWord.length; i++) {
     const answerKey = document.createElement("div")
     answerKey.classList = "answerkeys";
 
-
     // show answer
     // answerKey.innerHTML = randomWord.substring(i).substring(0,1);
 
@@ -26,12 +33,15 @@ for (let i = 0; i < randomWord.length; i++) {
     anskeysContainer.appendChild(answerKey)
 }
 //KEYS WRITE
+
 for (let i = 0; i < keys.length; i++) {
+
     const keysDiv = document.createElement("div")
     keysDiv.classList = "keystyle";
     keysDiv.innerHTML = keys[i]
     keysContainer.appendChild(keysDiv)
 }
+
 ansDiv.classList = "answer";
 gameContainer.classList = "game-container";
 ansDiv.innerHTML = randomWord;
@@ -41,3 +51,23 @@ container.appendChild(gcontainer)
 gcontainer.appendChild(gameContainer)
 gcontainer.appendChild(anskeysContainer)
 container.appendChild(keysContainer)
+
+const keystyle = document.querySelectorAll(".keystyle")
+for (let i = 0; i < keys.length; i++) {
+    keystyle[i].addEventListener("click", (e) => {
+        const clickItem = e.target.textContent;
+        var randomWordArray = []
+        for (let i = 0; i < randomWord.length; i++) {
+            randomWordArray.push(randomWord.substring(i).substring(0,1))         
+        }
+       
+            if (randomWordArray.includes(clickItem)) {
+                keystyle[i].style.background = "darkgreen"
+                console.log("var");
+            }else{
+                keystyle[i].style.opacity = "0.20"
+            }
+       
+    }) 
+
+}
