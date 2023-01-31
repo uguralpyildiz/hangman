23237439f7161cc5a76fcf5f49957b21a67691ce
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2023-01-31 12:59:00 +0300

    lorem ipsum

--- app.js
+++ app.js
@@ -1,93 +1,108 @@
-const container = document.querySelector(".container")
-const ansDiv = document.createElement("div")
-const gcontainer = document.createElement("div")
-gcontainer.classList = "gamecontainer"
-const gameContainer = document.createElement("div")
-//WORDS
-const words = ["git", "foreach", "format", "status", "facebook","instagram","snapchat", "twitter", "jeffbezos", "tesla", "spacex"]
-//KEYS
-const keys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+function index() {
+    const container = document.querySelector(".container")
+    const ansDiv = document.createElement("div")
+    const gcontainer = document.createElement("div")
+    gcontainer.classList = "gamecontainer"
+    const gameContainer = document.createElement("div")
+    //WORDS
+    const words = ["git", "foreach", "format", "status", "facebook", "instagram", "snapchat", "twitter", "jeffbezos", "tesla", "spacex"]
+    //KEYS
+    const keys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    // Random numbGenerator
+    function randomNumb() {
+        let randomer = Math.floor(Math.random() * words.length)
+        return randomer;
+    }
 
-function randomNumb() {
-    let randomer = Math.floor(Math.random() * words.length)
-    return randomer;
-}
+    //html areas-----------------------------------------
+
+    const randomWord = words[randomNumb()].toUpperCase()
+    const keysContainer = document.createElement("div")
+    keysContainer.classList = "key-container";
+
+    const anskeysContainer = document.createElement("div")
+    anskeysContainer.classList = "anskey-container";
+
+    for (let i = 0; i < randomWord.length; i++) {
+        const answerKey = document.createElement("div")
+        answerKey.classList = "answerkeys";
+        anskeysContainer.appendChild(answerKey)
+        answerKey.innerHTML = `<span class="ans-set">${`${randomWord.substring(i).substring(0, 1)}`}</span>`;
+    }
+
+    //create html----------------------------------------
+    for (let i = 0; i < keys.length; i++) {
+
+        const keysDiv = document.createElement("div")
+        keysDiv.classList = "keystyle";
+        keysDiv.innerHTML = keys[i]
+        keysContainer.appendChild(keysDiv)
+    }
+
+    ansDiv.classList = "answer";
+    gameContainer.classList = "game-container";
+    ansDiv.innerHTML = randomWord;
+
+    container.appendChild(ansDiv)
+    container.appendChild(gcontainer)
+    gcontainer.appendChild(gameContainer)
+    gcontainer.appendChild(anskeysContainer)
+    container.appendChild(keysContainer)
+    //----------------------------------------
 
 
+    //answewr array----------------------------------------
+    const keystyle = document.querySelectorAll(".keystyle")
+    const answerkeys = document.querySelectorAll(".ans-set")
+    const answerArray = [];
+    function hiddenAns() {
+        for (let i = 0; i < answerkeys.length; i++) {
+            answerkeys[i].style.display = "none";
+            answerArray.push(answerkeys[i].textContent)
+        }
 
-const randomWord = words[randomNumb()].toUpperCase()
-const keysContainer = document.createElement("div")
-keysContainer.classList = "key-container";
+    }
 
-const anskeysContainer = document.createElement("div")
-anskeysContainer.classList = "anskey-container";
+    hiddenAns()
+    //--------------------------------------------------------
 
-for (let i = 0; i < randomWord.length; i++) {
-    const answerKey = document.createElement("div")
-    answerKey.classList = "answerkeys";
-    anskeysContainer.appendChild(answerKey)
-    answerKey.innerHTML = randomWord.substring(i).substring(0, 1);
-}
-//KEYS WRITE
+    var ansCount = 0;
 
-for (let i = 0; i < keys.length; i++) {
+    //Index ------------------------------------------------
+    for (let i = 0; i < keys.length; i++) {
+        keystyle[i].addEventListener("click", (e) => {
 
-    const keysDiv = document.createElement("div")
-    keysDiv.classList = "keystyle";
-    keysDiv.innerHTML = keys[i]
-    keysContainer.appendChild(keysDiv)
-}
+            const clickItem = e.target.textContent;
 
-ansDiv.classList = "answer";
-gameContainer.classList = "game-container";
-ansDiv.innerHTML = randomWord;
 
-container.appendChild(ansDiv)
-container.appendChild(gcontainer)
-gcontainer.appendChild(gameContainer)
-gcontainer.appendChild(anskeysContainer)
-container.appendChild(keysContainer)
+            var randomWordArray = []
+            for (let i = 0; i < randomWord.length; i++) {
+                randomWordArray.push(randomWord.substring(i).substring(0, 1))
+            }
 
-const keystyle = document.querySelectorAll(".keystyle")
-const answerkeys = document.querySelectorAll(".answerkeys")
-const answerArray = [];
-function hiddenAns() {
-    for (let i = 0; i < answerkeys.length; i++) {
-        answerkeys[i].style.opacity = "0.2";
-        answerArray.push(answerkeys[i].textContent)
+            for (let i = 0; i < answerkeys.length; i++) {
+                if (clickItem === answerArray[i]) {
+                    answerkeys[i].style.display = "block"
+                    ansCount++;
+                }
+
+            }
+
+            if (randomWordArray.includes(clickItem)) {
+                keystyle[i].style.background = "darkgreen";
+            } else {
+                keystyle[i].style.opacity = "0.20"
+            }
+
+            if (randomWord.length === ansCount) {
+                console.log("reset");
+                
+            }
+
+        })
     }
 }
+index()
 
-hiddenAns()
+//--------------------------------------------------------
 
-for (let i = 0; i < keys.length; i++) {
-    keystyle[i].addEventListener("click", (e) => {
-        const clickItem = e.target.textContent;
-        var randomWordArray = []
-        for (let i = 0; i < randomWord.length; i++) {
-            randomWordArray.push(randomWord.substring(i).substring(0,1))     
-        }
-        
-        console.log(clickItem);
-        console.log(answerArray[1]);
-
-        for (let i = 0; i < answerkeys.length; i++) {
-            if (clickItem === answerArray[i]) {
-                answerkeys[i].style.opacity = "1"
-            }
-            
-        }
-
-
-       
-            if (randomWordArray.includes(clickItem)) {
-                keystyle[i].style.background = "darkgreen"
-                           
-            }else{
-                keystyle[i].style.opacity = "0.20"
-            }        
-     
-    }) 
-}
-
-// 
